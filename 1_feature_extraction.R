d2df448f64e6656185a54002151ca9409efd0628
MaVe0 <martin.verner@gmx.ch>
2022-08-02 22:17:01 +0200

    new strategy

--- 1_feature_extraction.R
+++ 1_feature_extraction.R
@@ -11,9 +11,31 @@ library(GGally)
 library(sjmisc)
 library(caret)
 library(LanguageToolR)
+library(data.table)
+library(stringi)
+library(koRpus)
+library(koRpus.lang.en)
+library(quanteda)
+library(quanteda.textstats)
 
 # repeat POS tagging?
 rePOS <- FALSE
+# repeat grammar check?
+reGrammar <- FALSE
+# repear redability score calculation?
+reRead <- FALSE
+
+# data <- fread("training_set_rel3.tsv") %>%
+#   filter(essay_set == 7) %>%
+#   select(essay,
+#          idea1 = rater1_trait1, idea2 = rater2_trait1,
+#          orga1 = rater1_trait2, orga2 = rater2_trait2,
+#          style1 = rater1_trait3, style2 = rater2_trait3,
+#          conv1 = rater1_trait4, conv2 = rater2_trait4,
+#          tot1 = rater1_domain1, tot2 = rater2_domain1)
+
+
+
 
 # Read and prepare data
 data <- read_xlsx("training_set_rel3.xlsx", col_types = c("numeric", "numeric", "text", rep("numeric", 25))) %>%
@@ -24,6 +46,10 @@ data <- read_xlsx("training_set_rel3.xlsx", col_types = c("numeric", "numeric", 
          style1 = rater1_trait3, style2 = rater2_trait3,
          conv1 = rater1_trait4, conv2 = rater2_trait4,
          tot1 = rater1_domain1, tot2 = rater2_domain1)
+
+# Replace ’ with '
+data <- mutate(data,
+               essay = str_replace_all(essay, "’", "'"))
 
 # Interrater reliability
 # cor(data$tot1, data$tot2)
@@ -107,6 +133,7 @@ nWord <- wordsR %>%
   unique() %>%
   mutate(nWordUnq = n()) %>%
   select(id, nWord, nWordUnq) %>%
+  mutate(TTR = round(nWordUnq/nWord, 2)) %>%
   unique()
 
 # without @words: number of words, average word length, number of characters, % of short words, % of long words
@@ -145,12 +172,12 @@ sentences <- unnest_tokens(select(dataN, id, essay),
 # remove stop words
 # View(stop_words)
 # which stop words dictionary to use? all lexica remove 72% of words
-stop_words <- stop_words %>%
+stop_words_sb <- stop_words %>%
   filter(lexicon == "snowball")
 
 # based on unique words without stop words: average word length, word count, % of short and long words
 nWordNSunq <- wordsN %>%
-  filter(!word %in% stop_words$word) %>%
+  filter(!word %in% stop_words_sb$word) %>%
   mutate(wordLength = nchar(word),
          tile20 = ntile(wordLength, 20),
          shortWord = tile20 == 1,
@@ -176,61 +203,131 @@ nWordNS <- wordsN %>%
             nCharTotalNS = sum(wordLength),
             nShortWordNS = sum(shortWord),
             nLongWordNS = sum(longWord)) %>%
+  ungroup() %>%
   # join the same thing based on unique words
   left_join(nWordNSunq, by = "id")
 
 # join to df containing word numbers with stop words and characters df => JOIN TO FEATURES
 words <- left_join(nWord, characters, by = "id") %>%
-  left_join(nWordNS, by = "id")
+  left_join(nWordNS, by = "id") %>%
+  mutate(TTRNS = round(nWordUnqNS/nWordNS, 2))
 
 # spell checking ###########################################################################
 spellCheck <- wordsN %>%
   mutate(correct = hunspell_check(word))
 
-# percentage of incorrect words => JOIN TO FEATURES
-nIncorrect <- spellCheck %>%
+# percentage of Inc words => JOIN TO FEATURES
+nInc <- spellCheck %>%
   group_by(id) %>%
-  summarise(nIncorrectWords = sum(!correct))
+  summarise(nIncWords = sum(!correct))
 
-# replace incorrect words with correct ones for other analyses?
-# incorrectWords <- filter(spellCheck, !correct) %>%
+# replace Inc words with correct ones for other analyses?
+# IncWords <- filter(spellCheck, !correct) %>%
 #   mutate(suggestion = "")
-# for(i in 1:nrow(incorrectWords)) {
-#   incorrectWords[i, "suggestion"] <- hunspell_suggest(pull(incorrectWords[i, "word"]))[[1]][1]
+# for(i in 1:nrow(IncWords)) {
+#   IncWords[i, "suggestion"] <- hunspell_suggest(pull(IncWords[i, "word"]))[[1]][1]
 # }
-# suggestions <- hunspell_suggest(incorrectWords$word)
+# suggestions <- hunspell_suggest(IncWords$word)
 # unnest(suggestions)
 
 # Grammar Check ########################################################################################################################
-test <- unnest_tokens(select(data, id, essay),
-                           output = "sentence",
-                           input = essay,
-                           token = "sentences",
-                      to_lower = FALSE)
-test <- test[1:12,]
 
-grammar <- data.frame(NULL)
-for(i in 1:nrow(test)) {
-  id <- pull(test[i, "id"])
-  text <- pull(test[i, "sentence"])
-  check <- LanguageToolR::languagetool(text)
-  check$id <- id
-  grammar <- rbind(grammar, check)
+if(reGrammar) {
+  # tokenize raw essays
+  test <- unnest_tokens(select(data, id, essay),
+                        output = "sentence",
+                        input = essay,
+                        token = "sentences",
+                        to_lower = FALSE) %>%
+    mutate(nChar = nchar(sentence),
+           nWord = str_count(sentence, '\\w+')) %>%
+    arrange(desc(nWord))
+
+  # Function splitting extremely long sentences
+  splitInParts <- function(string, size){
+    pat <- paste0('(?<=.{',size,'})')
+    strsplit(string, pat, perl=TRUE)
+  }
+
+  # extract very long sentences and split them up while keeping id
+  longSent <- test %>%
+    filter(nChar > 250)
+
+  longSentSplit <- data.frame(NULL)
+  for (i in 1:nrow(longSent)) {
+    text <- pull(longSent[i, "sentence"])
+    split <- splitInParts(text, 250)
+    df <- data.frame(sentence = split[[1]])
+    df$id <- pull(longSent[i, "id"])
+    longSentSplit <- rbind(longSentSplit, df)
+  }
+
+  # remove long sentences from main df and rbind split sentences
+  test2 <- test %>%
+    filter(nChar <= 250) %>%
+    select(id, sentence) %>%
+    rbind(longSentSplit) %>%
+    mutate(nChar = nchar(sentence),
+           nWord = str_count(sentence, '\\w+')) %>%
+    arrange(desc(nChar))
+
+  # Loop checking each sentence; write errors if languagetools doesn't work
+  grammar <- data.frame(NULL)
+  for(i in 1:nrow(test2)) {
+    # get id
+    id <- pull(test2[i, "id"])
+    # pull sentence
+    text <- pull(test2[i, "sentence"])
+
+    # check if languagetool works - if not rbind error message
+    check <- tryCatch({
+      LanguageToolR::languagetool(text)
+    }, warning = function(w) {
+      print("warning")
+    }, error = function(e) {
+
+      split <- splitInParts(testText, 50)[[1]]
+      check <- data.frame(NULL)
+      for(ii in length(split)) {
+        x <- LanguageToolR::languagetool(split[ii])
+        check <- rbind(split, x)
+      }
+      return(check)
+
+    }, finally = {
+    })
+
+    # add id
+    check$id <- id
+    # rbind to all language tool checks
+    grammar <- rbind(grammar, check)
+  }
+
+  # Save grammar check
+  saveRDS(grammar, "grammar_check_2.RDS")
 }
 
-LanguageToolR::languagetool("Patience is when your waiting .
-                            ")
-
-
-LanguageToolR::languagetool(test)
-
-
-bla <- data.frame(NULL)
-for(i in 1:4){
-  y <- bla <- LanguageToolR::languagetool(test[i, "x"])
-  bla <- rbind(bla, y)
-}
-
+# Create grammar fetaures => JOIN TO FEATURES!
+grammar <- readRDS("grammar_check_2.RDS") %>%
+  filter(rule_category_id %in% c("CASING", "CONFUSED_WORDS", "GRAMMAR", "PUNCTUATION", "TYPOGRAPHY", "TYPOS")) %>%
+  group_by(id, rule_category_id) %>%
+  mutate(n = n()) %>%
+  select(id, rule_category_id, n) %>%
+  unique() %>%
+  arrange(id) %>%
+  pivot_wider(id_cols = id, names_from = rule_category_id, values_from = n) %>%
+  mutate(across(everything(), ~ifelse(is.na(.), 0, .))) %>%
+  # relative to nWords
+  left_join(nWord, by = "id") %>%
+  mutate(
+    pIncWord2 = round(TYPOS/nWord, 5),
+    pIncCase = round(CASING/nWord, 5),
+    pIncConf = round(CONFUSED_WORDS/nWord, 5),
+    pIncGrammar = round(CONFUSED_WORDS/nWord, 5),
+    pIncPunct = round(PUNCTUATION/nWord, 5),
+    pIncTypog = round(TYPOGRAPHY/nWord, 5)
+  ) %>%
+  select(id, starts_with("pI"))
 
 # POS tagging ##########################################################################################################################
 if(rePOS) {
@@ -274,9 +371,140 @@ posShares <- posDF %>%
   summarise(n = n()) %>%
   mutate(sum = sum(n),
          percTag = round(100 * n/sum, 1)) %>%
+  ungroup() %>%
   select(id, tag, percTag) %>%
   pivot_wider(id_cols = id, names_from = tag, values_from = percTag) %>%
-  mutate(across(everything(), ~ifelse(is.na(.), 0, .)))
+  mutate(across(everything(), ~ifelse(is.na(.), 0, .))) %>%
+  mutate(nPOStags = rowSums(select(., -id) > 0))
+
+# Lexical Densities ##########################################################################################
+
+# Lexical density
+lexDens <- posDF %>%
+  mutate(id = as.numeric(str_replace(doc_id, "doc", ""))) %>%
+  filter(upos %in% c("NOUN", "ADJ", "VERB", "ADV")) %>%
+  group_by(id) %>%
+  summarise(nLexItems = n()) %>%
+  left_join(nWord, by = "id") %>%
+  mutate(lexDens = round(nLexItems/nWord, 2)) %>%
+  select(id, lexDens)
+
+# get share of non-stop-words as defined by other lexica (SMART, onix)
+stop_words_SMART <- stop_words %>%
+  filter(lexicon == "SMART")
+
+smartDens <- wordsN %>%
+  filter(!word %in% stop_words_SMART$word) %>%
+  group_by(id) %>%
+  summarise(n = n()) %>%
+  left_join(nWord, by = "id") %>%
+  mutate(smartDens = round(n/nWord, 2)) %>%
+  select(id, smartDens)
+
+stop_words_onix <- stop_words %>%
+  filter(lexicon == "onix")
+
+onixDens <- wordsN %>%
+  filter(!word %in% stop_words_onix$word) %>%
+  group_by(id) %>%
+  summarise(n = n()) %>%
+  left_join(nWord, by = "id") %>%
+  mutate(onixDens = round(n/nWord, 2)) %>%
+  select(id, onixDens)
+
+# join densities => JOIN TO FEATURES
+densities <- lexDens %>%
+  left_join(smartDens, by = "id") %>%
+  left_join(onixDens, by = "id")
+
+# sentiments #######################################################################
+nrc <- get_sentiments("nrc")
+
+# => JOIN TO FEATURES!
+sentiments <- wordsN %>%
+  left_join(nrc, by = "word") %>%
+  group_by(id, sentiment) %>%
+  summarise(n = n()) %>%
+  ungroup() %>%
+  pivot_wider(id_cols = id, names_from = sentiment, values_from = n) %>%
+  mutate(across(everything(), ~ifelse(is.na(.), 0, .))) %>%
+  left_join(nWord, by = "id") %>%
+  mutate(
+    angerScore = round(100*anger/nWord, 2),
+    anticScore = round(100*anticipation/nWord, 2),
+    disgustScore = round(100*disgust/nWord, 2),
+    fearScore = round(100*fear/nWord, 2),
+    joyScore = round(100*joy/nWord, 2),
+    negativeScore = round(100*negative/nWord, 2),
+    positiveScore = round(100*positive/nWord, 2),
+    sadScore = round(100*sadness/nWord, 2),
+    surpScore = round(100*surprise/nWord, 2),
+    trustScore = round(100*trust/nWord, 2)
+  ) %>%
+  select(id, ends_with("Score")) %>%
+  mutate(
+    sumEmotions = rowSums(select(., ends_with("Score"))),
+    nEmotions = rowSums(select(., ends_with("Score")) > 0)
+  )
+
+##########################################################################################
+# KoRpus package?
+# Unclear how to identify texts after taggin multiple texts => tagging within loops
+
+# koRpus tagging function
+tagEssay <- function(x) {
+  taggedText <- treetag(
+    x,
+    format = "obj",
+    treetagger="manual",
+    lang="en",
+    TT.options=list(
+      path="C:/TreeTagger",
+      preset="en"
+    )
+  )
+}
+
+# Readability ############################################################################
+
+if(reRead) {
+
+  # get empty df with readability index names
+  rInd <- readability(tagEssay(pull(data[1:5, "essay"])))
+  rInd <- data.frame(
+    index = names(rInd[1:31]),
+    value = rInd[1:31]
+  ) %>%
+    mutate(subID = 1) %>%
+    pivot_wider(subID, names_from = index, values_from = value) %>%
+    filter(subID == 999)
+
+  # loop extracting readability indices
+  essays <- data$essay
+
+  for(i in 1:length(essays)){
+    taggedEssay <- tagEssay(pull(data[i, "essay"]))
+    x <- readability(taggedEssay)
+    x <- data.frame(
+      index = names(x[1:31]),
+      value = x[1:31]
+    ) %>%
+      mutate(subID = i) %>%
+      pivot_wider(subID, names_from = index, values_from = value)
+    rInd <- rbind(rInd, x)
+  }
+
+  saveRDS(rInd, "readabilityScores.RDS")
+
+}
+
+# JOIN TO FEATURES!
+readability <-readRDS("readabilityScores.RDS") %>%
+  rename(id = subID)
+
+
+
+
 
 # Join Features ##############################################################################
 features <- left_join(sentences, words, by = "id") %>%
@@ -284,8 +512,14 @@ features <- left_join(sentences, words, by = "id") %>%
          nWordPerSentenceNS = round(nWordNS/nSentence, 0),
          percStopWords = round((1 - nWordNS/nWord) * 100, 1)) %>%
   left_join(nWordUnqLem, by = "id") %>%
+  left_join(nInc, by = "id") %>%
   left_join(nComma, by = "id") %>%
-  left_join(posShares, by = "id")
+  left_join(posShares, by = "id") %>%
+  left_join(sentiments, by = "id") %>%
+  left_join(densities, by = "id") %>%
+  left_join(readability, by = "id") %>%
+  mutate(across(everything(), ~ifelse(is.na(.), 0, .)))
+
 
 # save features
 saveRDS(features, "features.RDS")
---
+++ 3a_exploration_STYLE_classification_UP.R
@@ -0,0 +1,388 @@
+library(caret)
+library(corrplot)
+library(recipes)
+library(glmnet)
+library(rsample)
+library(caTools)
+set.seed(1898)
+
+# get conv scores
+score <- readRDS("styleScores.RDS") %>%
+  rename(score = style) %>%
+  mutate(score = as.factor(ifelse(score == 3, "Y", "N")))
+
+
+# Splitting (separate by criteria due to stratified split)
+trainIndex <- createDataPartition(score$score, p = .7,
+                                  list = FALSE,
+                                  times = 1)
+
+data <- readRDS("features.RDS") %>%
+  mutate(id = row_number()) %>%
+  left_join(score, by = "id") %>%
+  select(-id)
+
+train <- data[trainIndex,]
+test <- data[-trainIndex,]
+
+# transformation test-set
+pp <- preProcess(select(test, -score),
+                 method = c("center", "scale", "YeoJohnson"))
+pp
+testT <- predict(pp, newdata = select(test, -score))
+testT$score <- test$score
+summary(testT)
+saveRDS(test, "convTest.RDS")
+
+# transformation training-set
+pp <- preProcess(select(train, -score),
+                     method = c("center", "scale", "YeoJohnson"))
+pp
+trainT <- predict(pp, newdata = select(train, -score))
+trainT$score <- train$score
+summary(trainT)
+
+# logistic regression
+
+trControl <- trainControl(method = 'repeatedcv',
+                          number = 5,
+                          repeats =  5,
+                          summaryFunction = twoClassSummary,
+                          classProbs = TRUE, # IMPORTANT!
+                          verboseIter = TRUE,
+                          search = 'random')
+
+logit.CV <- train(x = select(trainT, -score), y = trainT$score,
+                  method = 'glmnet',
+                  trControl = trControl,
+                  family = 'binomial' )
+
+
+testT$predProb <- predict(logit.CV, testT, type = "prob")
+testT$predScore <- predict(logit.CV, testT)
+
+colAUC(testT$predProb, testT$score, plotROC = TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
+# INTERACTIONS
+
+# main effects recipe
+main_rec <- recipe(score ~., train) %>%
+  step_zv(all_predictors()) %>%
+  step_center(all_predictors()) %>%
+  step_scale(all_predictors())
+
+# interaction terms
+int_vars <- colnames(select(train, -score))
+
+interactions <- t(combn(as.character(int_vars), 2)) %>%
+  as_tibble() %>%
+  mutate(term = paste0(V1, ":", V2)) %>%
+  filter(row_number() %in% 1500:1711) %>%
+  pull(term) %>%
+  paste(collapse = "+")
+
+
+
+
+
+
+interactions <- paste(
+  "TTR:nWordN",
+  "PUNCT:ADP",
+  "nPOStags:mCharUnqWord",
+  "PUNCT:nEmotions",
+  sep = "+"
+)
+
+
+
+
+
+interactions <- paste("~", interactions)
+interactions <- as.formula(interactions)
+
+int_rec <- recipe(score ~., train) %>%
+  step_interact(interactions) %>%
+  step_zv(all_predictors()) %>%
+  step_center(all_predictors()) %>%
+  step_scale(all_predictors())
+
+# train models
+train_folds <- vfold_cv(train)
+train_ind <- rsample2caret(train_folds)
+
+ctrl <-
+  trainControl(
+    method = "cv",
+    index = train_ind$index,
+    indexOut = train_ind$indexOut
+  )
+
+glmn_grid <- expand.grid(alpha = seq(.2, 1, by = .2), lambda = 10^seq(-4, -1, by = 0.1))
+
+main_glmn <-
+  train(main_rec,
+        data = train,
+        method = "glmnet",
+        tuneGrid = glmn_grid,
+        trControl = ctrl
+  )
+
+int_glmn <-
+  train(int_rec,
+        data = train,
+        method = "glmnet",
+        tuneGrid = glmn_grid,
+        trControl = ctrl
+  )
+
+main_glmn$bestTune
+int_glmn$bestTune
+
+tune_plot <-
+  int_glmn %>%
+  pluck("results") %>%
+  mutate(alpha = factor(alpha)) %>%
+  ggplot(aes(x = lambda, y = RMSE, col = alpha)) +
+  geom_line() +
+  geom_point() +
+  scale_x_log10() +
+  ggtitle("(a)") +
+  theme(
+    legend.position = c(0.50, 0.75),
+    legend.background = element_blank()
+  ) +
+  labs(
+    colour = "Mixing Percentage",
+    x = "Regularization Parameter",
+    y = "RMSE (Cross-Validation)"
+  )
+
+
+lasso_int_coefs <-
+  int_glmn %>%
+  pluck("finalModel") %>%
+  coef(s = int_glmn %>% pluck("bestTune") %>% pluck("lambda")) %>%
+  as.matrix() %>%
+  as.data.frame() %>%
+  setNames("Coefficient") %>%
+  rownames_to_column("Predictor") %>%
+  dplyr::filter(grepl("_x_", Predictor)) %>%
+  arrange(desc(abs(Coefficient))) %>%
+  dplyr::filter(Predictor != "(Intercept)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Recursive Feature Elimination ################################################
+
+control <- rfeControl(functions = lmFuncs, # random forest
+                      method = "repeatedcv", # repeated cv
+                      repeats = 5, # number of repeats
+                      number = 10) # number of folds
+
+rfe1 <- rfe(x = select(trainT, -score),
+            y = trainT$score,
+            sizes = c(10:20, 30, 40, 50, 60, 70, 80, 90),
+            rfeControl = control)
+
+# Print the results
+rfe1
+
+# Print the selected features
+predictors(rfe1)
+
+# Print the results visually
+ggplot(data = rfe1, metric = "RMSE") + theme_bw()
+
+# variable importance
+varimp_data <- data.frame(feature = row.names(varImp(rfe1))[1:17],
+                          importance = varImp(rfe1)[1:17, 1])
+
+ggplot(data = varimp_data,
+       aes(x = reorder(feature, -importance), y = importance, fill = feature)) +
+  geom_bar(stat="identity") + labs(x = "Features", y = "Variable Importance") +
+  geom_text(aes(label = round(importance, 2)), vjust=1.6, color="white", size=4) +
+  theme_bw() + theme(legend.position = "none")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Perform Boruta search
+library(Boruta)
+boruta_output <- Boruta(score ~ ., trainT, doTrace=2)
+boruta_signif <- getSelectedAttributes(boruta_output, withTentative = TRUE)
+print(boruta_signif)
+roughFixMod <- TentativeRoughFix(boruta_output)
+boruta_signif <- getSelectedAttributes(roughFixMod)
+print(boruta_signif)
+imps <- attStats(roughFixMod)
+imps2 = imps[imps$decision != 'Rejected', c('meanImp', 'decision')]
+head(imps2[order(-imps2$meanImp), ])  # descending sort
+plot(boruta_output, cex.axis=.7, las=2, xlab="", main="Variable Importance")
+
+
+# Fit lm model using 5 x 5-fold CV: model
+model <- train(
+  score ~ .,
+  trainT,
+  method = "lm",
+  trControl = trainControl(
+    method = "repeatedcv",
+    number = 10,
+    repeats = 10,
+    verboseIter = TRUE
+  )
+)
+
+varImp(model)
+
+
+
+# Exploration
+
+# load the library
+library(mlbench)
+
+set.seed(10)
+
+subsets <- c(1:5, 10, 15, 20, 25)
+
+ctrl <- rfeControl(functions = lmFuncs,
+                   method = "repeatedcv",
+                   repeats = 5,
+                   verbose = FALSE)
+
+x <- select(trainT, -score)
+y <- trainT$score
+
+lmProfile <- rfe(x,
+                 y,
+                 sizes = subsets,
+                 rfeControl = ctrl)
+
+predictors(lmProfile)
+
+trainT <- select(trainT, all_of(predictors(lmProfile)))
+trainT$score <- train$score
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+library(gbm)
+model2 <- train(score ~ .,
+                trainT,
+                method = "gbm",
+                # trControl = fitControl,
+                # ## This last option is actually one
+                # ## for gbm() that passes through
+                verbose = FALSE
+                )
+
+
+# Print model to console
+model2
+
+
+testT$predScore <- predict(model, testT)
+sum(testT$score)
+sum(testT$predScore)
+
+
+
+
+
+
+
